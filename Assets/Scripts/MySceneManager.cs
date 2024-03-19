85110635534a125acb4dedfc5645f44d9b43d4a0
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2024-03-19 17:45:41 +0900

    Add button script

--- Assets/Scripts/MySceneManager.cs
+++ Assets/Scripts/MySceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class MySceneManager : MonoBehaviour
 {
     private static List<string> _scene_names = new List<string> {"PickingScene", "AssemblyScene"};
+    private static string _current_scene_name = "PickingScene";
 
     void Awake()
     {
@@ -20,17 +21,37 @@ public class MySceneManager : MonoBehaviour
     void Start()
     {
         foreach (var scene_name in _scene_names)
-        {
-            var root_objects = SceneManager.GetSceneByName(scene_name).GetRootGameObjects();
-            foreach (var root_object in root_objects)
-                root_object.SetActive(false);
-        }
-        
+            SetSceneVisibility(scene_name, scene_name == _current_scene_name);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwitchScene(string scene_name)
+    {
+        if (!_scene_names.Contains(scene_name))
+        {
+            Debug.Log("Scene[" + scene_name + "] not found!");
+            return;
+        }
+
+        if (scene_name == _current_scene_name)
+            return;
+
+        if (_current_scene_name != null)
+            SetSceneVisibility(_current_scene_name, false);
+        
+        _current_scene_name = scene_name;
+        SetSceneVisibility(_current_scene_name, true);
+    }
+
+    private void SetSceneVisibility(string scene_name, bool enable)
+    {
+        var root_objects = SceneManager.GetSceneByName(scene_name).GetRootGameObjects();
+        foreach (var root_object in root_objects)
+            root_object.SetActive(enable);
     }
 }
---
+++ Assets/Scripts/SwitchSceneButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSceneButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickButton()
+    {
+        var scene_name = text + "Scene";
+        MySceneManager.SwitchScene(scene_name);
+    }
+}
